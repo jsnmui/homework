8d028d1addae77e3a531cc570fdc951b08f71693
unknown <jasonmui2004@gmail.com>
2022-06-04 17:59:55 -0400

    Fibonacci.

--- server.js
+++ server.js
@@ -26,7 +26,7 @@
     let tip = req.params.total * req.params.tipPercentage/100
       res.send(`Tip is  ${tip} dollars.`)
   })
-  // magic 8 ball
+  // Magic 8 ball
   app.get('/magic/:question', (req, res) => {
     
     let response = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
@@ -37,8 +37,31 @@
   })
 
 
+  // Fibonacci 
+  app.get('/fibonacci/:number', (req, res) => {
+   
+     let fibTest = Number(req.params.number)
+     let fibResult = isFib(fibTest)
+
+     fibResult ? res.send("Very good. It is Fibonacci.")  :  res.send(" I can tell this is not a fibonacci number.")
+
+    
+  })
 
 
+  function isPerfectSquare(n) {
+      return n > 0 && Math.sqrt(n) % 1 === 0;
+  };
+  
+  //Equation modified from http://www.geeksforgeeks.org/check-number-fibonacci-number/ 
+
+  function isFib(number)
+  {
+      // numberToCheck is Fibinacci if one of 5*n*n + 4 or 5*n*n - 4 or both
+      // is a perferct square
+      return isPerfectSquare(5*number*number + 4) ||
+            isPerfectSquare(5*number*number - 4);
+  }
 
     // App Listener
   app.listen(PORT, () => {
